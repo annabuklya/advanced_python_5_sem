cbdcdf42a9be02af1c7241788b002775da116a5f
Anna Volkova <90874112+annabuklya@users.noreply.github.com>
2021-11-24 23:51:30 +0300

    Add files via upload

---
+++ add_flower.py
@@ -0,0 +1,20 @@
+!/usr/bin/env python
+coding: utf-8
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--name", required = True, type = str)
+parser.add_argument(" --country", required = True, type = str)
+parser.add_argument("--petal-colour", required = True, choices = ["R", "W", "Y", "V", "B"])
+parser.add_argument("--stem-length", reauired = True, type = int)
+parser.add_argument("--with-thorns", default = False, type = bool)
+parser.add_argument("--companion-plants", default = None, nargs='*')
+
+
+
+with open('journal.txt', 'a') as file: 
+    args = vars(parser.parse_args())
+    json.dump(args, file)
